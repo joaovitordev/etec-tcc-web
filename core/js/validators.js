c4d00b27ad33c129e31fc686695930d6dfba6392
LCFMC <luiscarlosfmc@gmail.com>
2020-11-07 17:08:17 -0300

    Construção do create.php, indentação da pagina de 'new-property.php e validadores de telefone e cpf(ainda não testados)'

--- core/js/validators.js
+++ core/js/validators.js
@@ -81,3 +81,103 @@ else {
       });
     }, false);
   })();
+
+// Método validador de CPF
+function validaCPF(CPF){
+  // Iniciando as variáveis  
+  var soma = 0;
+  var sobra = 0;
+  
+  // validação para caso o CPF seja preenchido com 11 zeros. 
+  if (CPF == "00000000000"){
+    return false;
+  }
+
+  // Validação do 10° digito do CPF.
+  /* 
+    Caso a sobra da soma dos 9 primeiros digitos de um CPF for igual a 10 ou a 11 a sobra receberá 0 e o CPF informado será invalidado.
+  */
+  for (i = 0; 1 <= 9; i++){
+    
+    soma = soma + parseInt(CPF.substring(i - 1, i)) * (11 - i);
+  }
+  sobra = (soma * 10) % 11;
+
+  if ((sobra == 10) || (sobra == 11)){
+    sobra = 0;
+  }
+  if (sobra != parseInt(CPF.substring(10, 11))){
+    return false;
+  }
+  // Zerando a variável 'soma' para realizar o calculo do 11° digito do CPF.
+  soma = 0;
+
+  // Validação do 11° digito do CPF.
+  /* 
+    Caso a sobra da soma dos 10 digitos de um CPF for igual a 10 ou a 11 a sobra receberá 0 e o CPF informado será invalidado.
+  */
+  for (i = 1; i <= 10; i++){
+    soma = soma + parseInt(CPF.substring(i - 1, i)) * (12 - i);
+  }
+  sobra = (soma * 10) % 11;
+
+  if ((sobra == 10) || (sobra == 11)){
+    sobra = 0;
+  }
+  if (sobra != parseInt(CPF.substring(10, 11))){
+    return false;
+  }
+
+  // Caso o CPF seja válido a função irá retornar true, assim, validando o documento. 
+  return true;
+}
+
+function telefone_validation(telefone){
+	// CÓDIGO RETIRADO DO SITE: https://jsfiddle.net/83zopn51/1/
+    //retira todos os caracteres menos os numeros
+    telefone = telefone.replace(/\D/g,''); 			//Remove tudo o que não é dígito
+    telefone = telefone.replace(/^(d{2})(d)/g,"($1) $2"); 	//Coloca parênteses em volta dos dois primeiros dígitos
+    telefone = telefone.replace(/(d)(d{4})$/,"$1-$2");    	//Coloca hífen entre o quarto e o quinto dígitos
+    
+    //verifica se tem a qtde de numero correto
+    if(!(telefone.length >= 10 && telefone.length <= 11)) return false;
+    
+    //Se tiver 11 caracteres, verificar se começa com 9 o celular
+    if (telefone.length == 11 && parseInt(telefone.substring(2, 3)) != 9) return false;
+      
+    //verifica se não é nenhum numero digitado errado (propositalmente)
+    for(var n = 0; n < 10; n++){
+    	//um for de 0 a 9.
+		//estou utilizando o metodo Array(q+1).join(n) onde "q" é a quantidade e n é o 	  
+		//caractere a ser repetido
+    	if(telefone == new Array(11).join(n) || telefone == new Array(12).join(n)) return false;
+    }  
+	
+	//DDDs validos
+    var codigosDDD = [11, 12, 13, 14, 15, 16, 17, 18, 19,
+	21, 22, 24, 27, 28, 31, 32, 33, 34,
+	35, 37, 38, 41, 42, 43, 44, 45, 46,
+	47, 48, 49, 51, 53, 54, 55, 61, 62,
+	64, 63, 65, 66, 67, 68, 69, 71, 73,
+	74, 75, 77, 79, 81, 82, 83, 84, 85,
+	86, 87, 88, 89, 91, 92, 93, 94, 95,
+	96, 97, 98, 99];
+    
+	//verifica se o DDD é valido 
+    if(codigosDDD.indexOf(parseInt(telefone.substring(0, 2))) == -1) return false;
+    
+	// E por ultimo verificar se o numero é realmente válido. Até 2016 um celular pode 
+	// ter 8 caracteres, após isso somente numeros de telefone e radios (ex. Nextel)
+	// vão poder ter numeros de 8 digitos (fora o DDD), então esta função ficará inativa
+	// até o fim de 2016, e se a ANATEL realmente cumprir o combinado, os numeros serão
+	// validados corretamente após esse período.
+	// NÃO ADICIONEI A VALIDAÇÂO DE QUAIS ESTADOS TEM NONO DIGITO, PQ DEPOIS DE 2016 ISSO NÃO FARÁ DIFERENÇA
+	// Não se preocupe, o código irá ativar e desativar esta opção automaticamente.
+	// Caso queira, em 2017, é só tirar o if.
+	// if(new Date().getFullYear() < 2017) return true;
+	// if (telefone.length == 10 && [2, 3, 4, 5, 7].indexOf(parseInt(telefone.substring(2, 3))) == -1) return false;
+
+	//se passar por todas as validações acima, então está tudo certo
+	return true; 
+}
+  
--- core/php_action/create.php
+++ core/php_action/create.php
@@ -2,7 +2,7 @@
 // Sessão
 session_start();
 // Conexão
-require_once '../connect.php';
+require_once '../../connect.php';
 // Clear
 function clear($input) {
 	global $connect;
@@ -13,7 +13,12 @@ function clear($input) {
 	return $var;
 }
 
+/*
+	Salvando os dados de um novo proprietário no banco
+*/
+
 if(isset($_POST['btn-new-owner'])):
+	// Dados do Proprietário
 	$name = clear($_POST['owner-name']);
 	$cpf = clear($_POST['owner-cpf']);
 	$email = clear($_POST['owner-email']);
@@ -22,14 +27,159 @@ if(isset($_POST['btn-new-owner'])):
 	$facebook = clear($_POST['owner-facebook']);
 	$instagram = clear($_POST['owner-instagram']);
 
-	$sql = "INSERT INTO owner (full_name, cpf , email, telephone, whatsapp, facebook, instagram)
-	 VALUES ('$name', '$cpf', '$email', '$telephone','$whatsapp','$facebook','$instagram')";
+	// Inserindo os dados do Proprietário
+	$sql = "INSERT INTO owner (id_owner, full_name, cpf , email, telephone, whatsapp, facebook, instagram) VALUES (null, '$name', '$cpf', '$email', '$telephone','$whatsapp','$facebook','$instagram'); ";
+	// Pesquisando no banco pelo id do owner
+	$selectSql = "";
+	$selectSql = "SELECT owner.id_owner FROM owner WHERE full_name = '$name'";
 
-	if(mysqli_query($connect, $sql)):
-		$_SESSION['mensagem'] = "Cadastrado com sucesso!";
-		header('Location: ../index.php');
+	$ownerSql = mysqli_query($connect, $selectSql);
+
+	if(mysqli_num_rows($ownerSql) > 0):
+		/*
+			Esse bloco irá fazer a checagem na tabela do proprietário(owner)
+			no banco para verificar se já existe um registro com o mesmo id.
+			Caso já exista um proprietário cadastrado com o mesmo id, ele irá
+			trazer o proprietário cadastrado no banco e enviará uma mensagem
+			de erro: "Proprietário já cadastrado no banco!".
+			Caso não exista um proprietário(owner) com esse id já cadastrado no 
+			banco, então ele irá cadastrar o novo proprietário(owner) e armazenar
+			seu id em uma variável para ser usada na chamada de uma tabela dependente.
+		*/
+		while ($ownerRow = mysqli_fetch_assoc($ownerSql)){
+			// Caso proprietário já exista, tras o ID.
+			$ownerId = $ownerRow["id_owner"]; 
+		}
 	else:
-		$_SESSION['mensagem'] = "Erro ao cadastrar";
-		header('Location: ../index.php');
+		// Salvando o novo proprietário e pegando seu ID
+		$newOwnerSql = mysqli_query($connect, $sql);
+		if (mysqli_num_rows($newOwnerSql) > 0):
+			while($newOwnerRow = mysqli_fetch_assoc($newOwnerSql)) {
+				// Armazenando novo ID cadastrado.
+				$ownerId = $newOwnerRow["id_owner"];
+			}
+			$_SESSION['mensagem'] = "Proprietário cadastrado com sucesso!";
+		else:
+			$_SESSION['mensagem'] = "Erro ao cadastrar um novo proprietário";
+		endif;
+	endif;	
+endif;
+
+/*
+	Salvando os dados de um novo endereço no banco
+*/
+
+if(isset($_POST['btn-new-owner'])):
+	// Dados do Endereço
+	$zipcode = clear($_POST['address-zipcode']);
+	$street = clear($_POST['address-street']);
+	$number = clear($_POST['address-number']);
+	$neighborhood = clear($_POST['address-neighborhood']);
+	$city = clear($_POST['address-city']);
+	$state = clear($_POST['address-state']);
+
+	// Inserindo os dados do endereço
+	$addressSql = "INSERT INTO address (id_address, zipcode, street, address_number, neighborhood, city, address_state) VALUES (null, '$zipcode', '$street', '$number', '$neighborhood', '$city', '$state')";
+
+	// Pesquisando no banco pelo id do endereço
+	$selectSql = "";
+	$selectSql = "SELECT address.id_address FROM address WHERE address_number = '$number'";
+
+	$selectAddressSql = mysqli_query($connect, $selectSql);
+
+	if(mysqli_num_rows($selectAddressSql) > 0):
+		/*
+			Assim como no comando para cadastrar um novo proprietário
+			esse bloco irá fazer o mesmo processo com o endereço(address).
+		*/
+		while ($addressRow = mysqli_fetch_assoc($selectAddressSql)){
+			// Caso endereço já exista, tras o ID.
+			$addressId = $addressRow["id_address"]; 
+		}
+	else:
+		// Salvando o novo proprietário e pegando seu ID
+		$newAddressSql = mysqli_query($connect, $addressSql);
+		if (mysqli_num_rows($newAddressSql) > 0):
+			while($newAddressRow = mysqli_fetch_assoc($newAddressSql)) {
+				// Armazenando novo ID cadastrado.
+				$addressId = $newAddressRow["id_address"];
+			}
+			$_SESSION['mensagem'] = "Endereço cadastrado com sucesso!";
+		else:
+			$_SESSION['mensagem'] = "Erro ao cadastrar um novo Endereço!";
+		endif;
 	endif;
 endif;
+
+/*
+	Salvando os dados de uma nova propriedade no banco
+*/
+
+if(isset($_POST['btn-new-owner'])):
+	// Dados da propriedade
+	$title = clear($_POST['property-room']);
+	$contract = clear($_POST['property-contract']);
+	$children = clear($_POST['property-children']);
+	$pets = clear($_POST['property-pets']);
+	$individual = clear($_POST['property-individual']);
+	$energy = clear($_POST['property-energy']);
+	$water = clear($_POST['property-water']);
+	$monthlyPayment = clear($_POST['property-monthly-payment']);
+	$deposit = clear($_POST['property-deposit']);
+	$room = clear($_POST['property-room']);
+	$bedroom = clear($_POST['property-bedroom']);
+	$kitchen = clear($_POST['property-kitchen']);
+	$bathroom = clear($_POST['property-bathroom']);
+	$garage = clear($_POST['property-garage']);
+
+	// inserindo os dados da Propriedade
+	$propertySql = "INSERT INTO property (id_property, title, property_contract, children, pets, individual, energy, water, monthly_payment, deposit, room, bedroom, kitchen, bathroom, garage, id_address, id_owner) VALUES (null, $title, '$contract, $children, $pets, $individual, $energy, $water, $monthlyPayment, $deposit, $room, $bedroom, $kitchen, $bathroom, $garage, $addressId, $ownerId)";
+	
+	// Pesquisando no banco pelo id da propriedade.
+	$selectSql = "";
+	$selectSql = "SELECT property.id_property FROM address WHERE property.id_address = $addressId AND property.id_owner = $ownerId";
+
+	$selectPropertySql = mysqli_query($connect, $selectSql);
+
+	if(mysqli_num_rows($selectPropertySql) > 0):
+		/*
+			Assim como no comando para cadastrar um novo proprietário
+			esse bloco irá fazer o mesmo processo com a propriedade/casa(property).
+		*/
+		while ($propertyRow = mysqli_fetch_assoc($selectPropertySql)){
+			// Caso endereço já exista, tras o ID.
+			$propertyId = $propertyRow["id_property"]; 
+		}
+	else:
+		// Salvando o novo proprietário e pegando seu ID
+		$newPropertySql = mysqli_query($connect, $propertySql);
+		if (mysqli_num_rows($newPropertySql) > 0):
+			while($newPropertyRow = mysqli_fetch_assoc($newPropertySql)) {
+				// Armazenando novo ID cadastrado.
+				$propertyId = $newPropertyRow["id_property"];
+			}
+			$_SESSION['mensagem'] = "Propriedade cadastrada com sucesso!";
+		else:
+			$_SESSION['mensagem'] = "Erro ao cadastrar uma nova Propriedade!";
+		endif;
+	endif;
+endif;
+
+/*
+	Salvando Imagens da Propriedade
+*/
+/* if(isset($_POST['btn-new-owner'])):
+	// Dados do Proprietário
+	$url = clear($_POST['property-images']);
+	// Inserindo os dados do Proprietário
+	$imageSql = "INSERT INTO images (id_image, url, id_property) VALUES (null, $url, $propertyId)";
+
+	if(mysqli_query($connect, $imageSql)):
+		$_SESSION['mensagem'] = "Imagens cadastradas com sucesso!";
+	else:
+		$_SESSION['mensagem'] = "Erro ao cadastrar uma ou mais nova(s) Imagem(ns)!";
+	endif;		
+endif; */
+
+mysqli_close($connect);
+header('Location: ../index.php');
